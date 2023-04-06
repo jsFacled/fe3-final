99e7b908a9979c72337943291475a25d0df2ae9d
Facundo Ledezma <54073355+jsFacled@users.noreply.github.com>
2023-04-06 02:44:40 -0300

    completando Fetch y map en Home

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const Card = ({ name, username, id }) => {
-
+  const [isFavorite, setIsFavorite] = useState(false);
   const addFav = ()=>{
     // Aqui iria la logica para agregar la Card en el localStorage
+    setIsFavorite(true);
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    if (!favorites.includes(id)) {
+      favorites.push(id);
+      localStorage.setItem('favorites', JSON.stringify(favorites));
+    }
   }
 
   return (
     <div className="card">
+      <Link to={`/dentist/${id}`}>
         {/* En cada card deberan mostrar en name - username y el id */}
 
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
+
+    {/* <img src="/images/doctor.jpg"
+          alt="doctor placeholder"/>
+       */}
+       <h1>{name}</h1>
+       <h2>{username}</h2>
+       <h3>{id}</h3>
+       </Link>
         <button onClick={addFav} className="favButton">Add fav</button>
     </div>
   );
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,17 +1,61 @@
-import React from 'react'
-import Card from '../Components/Card'
+import React, { useEffect, useState } from "react";
+import Card from "../Components/Card";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
+  //En un estado guardo resultado Fetch
+  const [data, setData] = useState();
+  //Guardo error posible
+  const [errorFetch, setErrorFetch] = useState();
+  //Muestro un loading..
+  const [loading, setLoading] = useState(true);
+
+  //Funcion Fetch
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+  async function handleFetchDataDentist() {
+    setLoading(true);
+    try {
+      const dentistFetch = await (await fetch(url)).json();
+      console.log("d...", dentistFetch);
+      setData(dentistFetch);
+    } catch (error) {
+      setErrorFetch(error.message);
+      console.log("Error en Fetching...", error.message);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    handleFetchDataDentist();
+  }, []);
+
+  //Return----------
   return (
-    <main className="" >
+    <main className="">
       <h1>Home</h1>
-      <div className='card-grid'>
+      <div className="card-grid">
         {/* Aqui deberias renderizar las cards */}
+        {loading ? (
+          <p>Loading...</p>
+        ) : errorFetch ? (
+          <p>Error: {errorFetch}</p>
+        ) : (
+          <div className="card-grid">
+            {data.map((dentist) => (
+              <Card
+                key={dentist.id}
+                id={dentist.id}
+                name={dentist.name}
+                username={dentist.username}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
