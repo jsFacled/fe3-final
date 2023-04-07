b250ee0d78d3496c11b682103d7f5277eb46759c
Facundo Ledezma <54073355+jsFacled@users.noreply.github.com>
2023-04-07 20:24:02 -0300

    en card favorita se agrega o quita la estrella y se guarda en local storage

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,28 +5,35 @@ import { Link } from "react-router-dom";
 
 
 const Card = ({ name, username, id }) => {
+
+
   const {theme}= useContext(ContextGlobal)
 
-  const [isFavorite, setIsFavorite] = useState(false);
-  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-  const isAlreadyFav = favorites.some(fav => fav.id === id);
-  
+  const [isFavorite, setIsFavorite] = useState()
 
-  const addFav = ()=>{
-    // Aqui iria la logica para agregar la Card en el localStorage
-    
-    if (!isAlreadyFav) {
+  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  const handleFav = () => {
+    const favoriteIndex = favorites.findIndex(fav => fav.id === id);
+    if (favoriteIndex === -1) {
+      // La Card no está en favoritos
       favorites.push({ name, username, id });
       localStorage.setItem('favorites', JSON.stringify(favorites));
-      setIsFavorite(true)
+      setIsFavorite(true);
+    } else {
+      // La Card está en favoritos
+      favorites.splice(favoriteIndex, 1);
+      localStorage.setItem('favorites', JSON.stringify(favorites));
+      setIsFavorite(false);
     }
   }
 
-  useEffect(() => {
-    if(favorites.some(fav => fav.id === id)){
-      setIsFavorite(true)
-    };
-  }, []);
+
+   useEffect(() => {
+     if(favorites.some(fav => fav.id === id)){
+       setIsFavorite(true)
+     };
+   }, [favorites, id]);
+   
   return (
     <div className= {`card ${theme}`}>
       <Link to={`/dentist/${id}`}>
@@ -43,7 +50,7 @@ const Card = ({ name, username, id }) => {
        <h2>{username}</h2>
        <h3>{id}</h3>
        </Link>
-       <button onClick={addFav} className={`favButton ${isFavorite ? 'favorite' : ''}`}>
+       <button onClick={handleFav} className={`favButton ${isFavorite ? 'favorite' : ''}`}>
   {isFavorite ? 'Added to favorites ⭐' : 'Add to favorites'}
 </button>
     </div>
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import "../index.css"
 
 import Card from "../Components/Card";
